ef5cee0ae2c24cb77cb61151c2add874063c6269
hau nguyen <ncthau@gmail.com>
2024-10-06 20:53:59 +0700

    commit

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -4,44 +4,33 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-import re
+from awsglue import DynamicFrame
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
+job.init(args['JOB_NAME'], args)
 
-# Script generated for node S3 bucket
-S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://haunct-data-lakehouse/customer/landing"],
-        "recurse": True,
-    },
-    transformation_ctx="S3bucket_node1",
-)
+# Script generated for node Customer Landing
+CustomerLanding_node1728142263995 = glueContext.create_dynamic_frame.from_catalog(database="haunctdb", table_name="customer_landing", transformation_ctx="CustomerLanding_node1728142263995")
 
-# Script generated for node PrivacyFilter
-PrivacyFilter_node1689302643870 = Filter.apply(
-    frame=S3bucket_node1,
-    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
-    transformation_ctx="PrivacyFilter_node1689302643870",
-)
+# Script generated for node Share with Research
+SqlQuery5206 = '''
+select * from myDataSource
+where sharewithresearchasofdate is not null;
+'''
+SharewithResearch_node1728142319582 = sparkSqlQuery(glueContext, query = SqlQuery5206, mapping = {"myDataSource":CustomerLanding_node1728142263995}, transformation_ctx = "SharewithResearch_node1728142319582")
 
-# Script generated for node Trusted Customer Zone
-TrustedCustomerZone_node1689302785270 = glueContext.write_dynamic_frame.from_options(
-    frame=PrivacyFilter_node1689302643870,
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "path": "s3://haunct-data-lakehouse/customer/trusted/",
-        "partitionKeys": [],
-    },
-    transformation_ctx="TrustedCustomerZone_node1689302785270",
-)
-
+# Script generated for node Customer Trusted
+CustomerTrusted_node1728142576406 = glueContext.getSink(path="s3://haunct-data-lakehouse/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="snappy", enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1728142576406")
+CustomerTrusted_node1728142576406.setCatalogInfo(catalogDatabase="haunctdb",catalogTableName="customer_trusted")
+CustomerTrusted_node1728142576406.setFormat("json")
+CustomerTrusted_node1728142576406.writeFrame(SharewithResearch_node1728142319582)
 job.commit()
